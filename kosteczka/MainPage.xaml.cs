0f6a7ff7729478ee6eeb5560a53411e1c6c71fb2
Patrycja Grzenkowicz <patrycja.grzenkowicz2@teb.edu.pl>
2024-09-23 21:12:56 +0200

    2/3

--- kosteczka/MainPage.xaml.cs
+++ kosteczka/MainPage.xaml.cs
@@ -12,8 +12,24 @@
         private void OnCounterClicked(object sender, EventArgs e)
         {
             Random r = new Random();
-            int diceRoll = r.Next(1, 7);
-            CounterBtn.Text = "Wynik rzutu: " + diceRoll.ToString();
+            int diceRoll = 6;
+            if (K4.IsChecked)
+            {
+                diceRoll = r.Next(1, 5);
+            }
+            if (K6.IsChecked)
+            {
+                diceRoll = r.Next(1, 7);
+            }
+            if (K10.IsChecked)
+            {
+                diceRoll = r.Next(1, 11);
+            }
+            if (K12.IsChecked)
+            {
+                diceRoll = r.Next(1, 13);
+            }
+            RollLabel.Text = "Wynik rzutu: " + diceRoll.ToString();
         }
     }
 
